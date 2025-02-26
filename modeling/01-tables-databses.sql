de1fa463f0b54e58e0710fb8b2a4e1a8604aae2a
kalet Chavez <kalet.svdev@gmail.com>
2025-02-26 01:25:11 -0600

    :tada: Initial commit

---
+++ modeling/01-tables-databses.sql
@@ -0,0 +1,16 @@
+/* Comandos DDL */
+
+--Comando CREATE el cual se usa para crear objetos de bases de datos( tablas, vistas e indices, etc.)
+CREATE DATABASE myDB(); -- Crea una base de datos con el nombre que se le indique.
+CREATE TABLE anyname(); -- Crea una tabla con los atributos que se le indican.
+
+--Comando ALTER el cual modifica objetos de base de datos existentes.
+
+ALTER TABLE anyname ADD COLUMN nueva_columna VARCHAR(10); -- agrega una columna nueva a la tabla.
+
+--Comando DROP el cual elimina objetos de bases de datos.
+
+DROP TABLE myDB; -- elimina la base de datos completa.
+
+/* Comandos DML */
+
